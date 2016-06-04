b6a92b72500b5c0398da059fc60347fd62ab9b69
mityok69 <ptdimitry@gmail.com>
2016-06-04 17:55:22 +0300

    added show control

--- js/directives/orientation-detector-directive.js
+++ js/directives/orientation-detector-directive.js
@@ -1,20 +1,32 @@
-mainApp.directive('orientationDetector',['$window',function($window){
+mainApp.directive('orientationDetector',['$window','$rootScope',function($window, $rootScope){
 	return {
 		restrict: 'EA',
 		scope:{},
 		template:'<div>{{orientation}} - ({{beta}},{{gamma}},{{alpha}})</div>',
 		controller:function($scope){
+			$scope.inclination = 0;
+			$scope.isUp = false;
 			function deviceorientation(e){
 				$scope.$applyAsync(function(){
+					//portrait (orientation 0) 0 to 90 continue 180
 					$scope.beta = Math.round(e.beta);
+					//landscape (orientation 90) 0 to -90 jump 90 to 0
 					$scope.gamma = Math.round(e.gamma);
 					$scope.alpha = Math.round(e.alpha);
+					if($scope.orientation == 0 && ($scope.beta > 15 && $scope.beta < 100)){
+						$scope.isUp = true;
+					}else{
+						if($scope.isUp && $rootScope.imgShow){
+							$rootScope.imgShow=false;
+						}
+						$scope.isUp = false;
+					}
 				});
 			}
 			function orientationchange(){
 				console.log('ch',screen.orientation.angle);
 				$scope.$applyAsync(function(){
-					$scope.orientation=screen.orientation.angle;
+					$scope.orientation = screen.orientation.angle;
 				});
 			}
 			$scope.init = function(){
