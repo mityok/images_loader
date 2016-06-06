f54d05a2101e73a1602e0e25f830d5e3265560b6
Dimitry Patt <dimitry.patt@hpe.com>
2016-06-06 12:26:10 +0300

    rating updated and exclude added to menu

--- js/directives/rating-directive.js
+++ js/directives/rating-directive.js
@@ -5,8 +5,6 @@ mainApp.directive('rating',['$window', '$routeParams','dataStorageService',funct
 		scope:{
 		},
 		controller:function($scope){
-			
-
 			$scope.$on('$routeChangeSuccess', checkItem);
 			function checkItem(){
 				$scope.itemId = $routeParams.itemId;
@@ -17,6 +15,15 @@ mainApp.directive('rating',['$window', '$routeParams','dataStorageService',funct
 			$scope.rate = function(i){
 				$scope.selectedItem.rating = i;
 				dataStorageService.setDebounceData(0,true);
+			}
+			$scope.exclude = function(i){
+				$scope.selectedItem.excluded = true;
+				dataStorageService.setDebounceData(0,true);
+				$location.path("/list");
+			}
+			$scope.getRatingIcon = function(i){
+				var arr=['fa-star text-danger', 'fa-star-half-o text-warning', 'fa-star-o text-info', 'fa-star-half-o text-success', ' fa-star text-primary'];
+				return arr[i + Math.floor(arr.length / 2)];
 			}
 		}
 	}
