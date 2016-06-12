417bae67b44215b85700f01c2723ad0b1173f998
mityok69 <ptdimitry@gmail.com>
2016-06-12 17:42:51 +0300

    fix zero bytes images save

--- server/write_post_thumb.php
+++ server/write_post_thumb.php
@@ -3,10 +3,13 @@ include_once("pass.php");
 $link = $_POST['link'];
 $url = $_POST['url'];
 $dir = $_POST['dir'];
-	$proxy = PasswordSingleton::getInstance()->getProxy();
-	$data = file_get_contents($url, false, $proxy ? stream_context_create(array('http'=>array('method'=>"GET",'proxy' => $proxy))) : NULL);
-	if(!is_dir($dir)){
-		mkdir($dir, 0777, true);
-	}
+$proxy = PasswordSingleton::getInstance()->getProxy();
+$data = file_get_contents($url, false, $proxy ? stream_context_create(array('http'=>array('method'=>"GET",'proxy' => $proxy))) : NULL);
+if(!is_dir($dir)){
+	mkdir($dir, 0777, true);
+}
+list($width, $height, $type, $attr) = getimagesize($url);
+if($data && $width > 0 && $height > 0){
 	file_put_contents($link, $data);
+}
 ?>
